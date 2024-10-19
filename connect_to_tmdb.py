ac48b73d3b9f38876269c312c8e2d9ea054b9d11
AlexanderDudek10 <alex@familiendudek.dk>
2024-10-19 04:00:32 +0200

    Setup + spongebob
    💀

---
+++ connect_to_tmdb.py
@@ -0,0 +1,5 @@
+import requests
+# TMDB API key
+API_KEY = 'c19b037e22194f3f3fdec719b1521f62'
+# Base URL for TMDB API
+BASE_URL = 'https://api.themoviedb.org/3'
---
+++ main.py
@@ -0,0 +1,6 @@
+# Load welcome message
+# after 5 s, clear screen
+# Load home/main screen 
+# ^ List + roulette ^
+import welcome
+welcome.my_function()
