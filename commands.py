82cf9d9ccbbae77eaada6c2d15ca0456802d5556
natalia <112906591+nataliadgalindo@users.noreply.github.com>
2024-10-24 03:40:46 +0100

    plan commands

---
+++ commands.py
@@ -0,0 +1,9 @@
+# \roulette
+# \add
+# \genre
+# \search - title, year, genres - \search -t title -y year -g genre,genre,genre
+
+
+                                                                                                            
+                                                                                                            
+                                                                                                            
