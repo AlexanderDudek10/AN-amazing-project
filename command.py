ba588d81e1b12ab4ad5b9a490dbb0980cf470d71
natalia <112906591+nataliadgalindo@users.noreply.github.com>
2024-10-24 03:42:58 +0100

    deleted command.py

--- command.py
+++
@@ -1,9 +0,0 @@
-# \roulette
-# \add
-# \genre
-# \search - title, year, genres - \search -t title -y year -g genre,genre,genre
-
-
-                                                                                                            
-                                                                                                            
-                                                                                                            
