a3c4fd0bf2ffe132f68c4ad91eeedbac60ea40df
natalia <112906591+nataliadgalindo@users.noreply.github.com>
2024-10-24 15:55:56 +0100

    created utils file

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-from variables import genre_dict
 import click
 
 
@@ -10,7 +9,7 @@ def display_movies(movies):
         genres = ", ".join(
             [genre_dict.get(genre_id, "Unknown") for genre_id in genre_ids]
         )
-        click.echo(f"- {movie['title']} ({year}) - {genres}\n")
+        click.echo(f"» {movie['id']} - {movie['title']} ({year}) - {genres}\n")
 
 
 def get_genre_ids(genres):
@@ -31,3 +30,26 @@ def get_genre_ids(genres):
             click.echo(f"Genre '{genre_name}' not found.")
 
     return genre_ids
+
+
+genre_dict = {
+    28: "action",
+    12: "adventure",
+    16: "animation",
+    35: "comedy",
+    80: "crime",
+    99: "documentary",
+    18: "drama",
+    10751: "family",
+    14: "fantasy",
+    36: "history",
+    27: "horror",
+    10402: "music",
+    9648: "mystery",
+    10749: "romance",
+    878: "science fiction",
+    10770: "tv movie",
+    53: "thriller",
+    10752: "war",
+    37: "western",
+}
