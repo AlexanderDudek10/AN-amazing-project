3b8eee1b756bc46a500908a9a1e89f1b62a0c323
natalia <112906591+nataliadgalindo@users.noreply.github.com>
2024-10-25 03:49:18 +0100

    added wishlist command and options

--- commands/wishlist.py
+++ commands/wishlist.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import click
+from connect_to_tmdb import new_wishlist_record
+import os
 
 
 @click.command()
@@ -10,8 +12,36 @@ def wishlist(view, id):
     Create a wishlist, add or delete movie records, check off movies you've seen and play roulette to choose which movie to watch next.
     """
 
+    df = None
+
+    if os.path.exists("wishlist.csv"):
+        df = pd.read_csv("wishlist.csv")
+
+    else:
+        data = {
+            "title": [],
+            "genres": [],
+            "year": [],
+            "runtime": [],
+        }
+
+        df = pd.DataFrame(data)
+        df.to_csv("wishlist.csv", index=True)
+
     if view and not id:
-        print("view")
+        print(df)
 
     elif id and not view:
         print(f"id: {id}")
+
+        df2 = new_wishlist_record(id)
+
+        if isinstance(df2, pd.DataFrame):
+            updated_df = pd.concat([df, df2], ignore_index=True)
+
+            updated_df["runtime"] = updated_df["runtime"].astype(int)
+            updated_df["id"] = updated_df["id"].astype(int)
+            updated_df = updated_df[["id", "title", "genres", "year", "runtime"]]
+            updated_df.to_csv("wishlist.csv", index=False)
+        else:
+            print("no")
